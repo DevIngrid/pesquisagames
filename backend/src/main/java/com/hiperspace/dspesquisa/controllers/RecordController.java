220edeff0433e98968a684977732e1fccb9dfb2d
Rita Nascimento <juliedoisj@gmail.com>
2021-12-27 05:28:06 -0300

    GET records paginado e filtro por data

--- backend/src/main/java/com/hiperspace/dspesquisa/controllers/RecordController.java
+++ backend/src/main/java/com/hiperspace/dspesquisa/controllers/RecordController.java
@@ -1,10 +1,18 @@
 package com.hiperspace.dspesquisa.controllers;
 
+import java.time.Instant;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hiperspace.dspesquisa.dto.RecordDTO;
@@ -22,8 +30,29 @@ public class RecordController {
 	public ResponseEntity<RecordDTO> insert(@RequestBody RecordInsertDTO dto) {
 		RecordDTO newDTO = service.insert(dto);
 		return ResponseEntity.ok().body(newDTO);
+	}
+	
+	//fiz um pouco diferente do que o Nélio fez - usei pageable como ele fez em outra aula.
+	@GetMapping
+	public ResponseEntity<Page<RecordDTO>> findAll(
+			@RequestParam(value = "min", defaultValue = "") String min,
+			@RequestParam(value = "max", defaultValue = "") String max,
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "0") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "moment") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "DESC") String direction, Pageable pageable) {
 		
+		Instant minDate = ("".equals(min)) ? null : Instant.parse(min);
+		Instant maxDate = ("".equals(max)) ? null : Instant.parse(max);
 		
+		if (linesPerPage == 0) {
+			linesPerPage = Integer.MAX_VALUE;
+		}
+		
+		pageable = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		Page<RecordDTO> pageList = service.findByMoments(minDate, maxDate, pageable);
+		return ResponseEntity.ok().body(pageList);
 	}
 
 }
--- backend/src/main/java/com/hiperspace/dspesquisa/repositories/RecordRepository.java
+++ backend/src/main/java/com/hiperspace/dspesquisa/repositories/RecordRepository.java
@@ -1,9 +1,24 @@
 package com.hiperspace.dspesquisa.repositories;
 
+import java.time.Instant;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.hiperspace.dspesquisa.entities.Record;
 
 public interface RecordRepository extends JpaRepository<Record, Long>{
+	
+	//coalesce(:min, null) e coalesce(:max, null) foi só para funcionar no Postgres
+	//do contrario seria :min is null e max is null
+	
+	@Query("select r from Record r where "
+			+ "(coalesce(:min, null) is null or r.moment >= :min) and "
+			+ "(coalesce(:max, null) is null or r.moment<= :max)")
+	Page<Record> findByMoments(Instant min, Instant max, Pageable pageable);
+
+	
 
 }
