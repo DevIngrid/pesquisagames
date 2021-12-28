4bceb2dbafb23b0a0992b94c9d2994268992235b
Rita Nascimento <juliedoisj@gmail.com>
2021-12-28 15:25:36 -0300

    Criando navegação com rotas

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,15 +1,9 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header";
-import Home from "./pages/Home";
+import Rotas from "./Rotas";
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Home />
-    </div>
-  );
+  return <Rotas />;
 }
 
 export default App;
--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { ReactComponent as ArrowIcon } from "../../assets/arrow.svg";
 import { ReactComponent as GamerImage } from "../../assets/gamer.svg";
+import Records from "../Records";
 import "./styles.css";
 
 const Home = () => (
@@ -11,12 +13,14 @@ const Home = () => (
         Clique no botão abaixo e saiba quais são os jogos que os gamers estão
         escolhendo!
       </h3>
-      <div className="home-actions">
-        <button className="home-btn">QUERO SABER QUAIS SÃO</button>
-        <div className="home-btn-icon">
-          <ArrowIcon />
+      <Link to="/records">
+        <div className="home-actions">
+          <button className="home-btn">QUERO SABER QUAIS SÃO</button>
+          <div className="home-btn-icon">
+            <ArrowIcon />
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
     <div className="home-image">
       <GamerImage />
